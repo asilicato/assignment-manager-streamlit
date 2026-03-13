81dec95f8f636902f6f90deb512838237d84b79f
asilicato <asilicat@udel.edu>
2026-03-13 11:16:54 -0400

    app_day6.py

---
+++ app_day2.py
@@ -0,0 +1,149 @@
+import streamlit as st
+import time 
+import json
+from pathlib import Path 
+
+st.title("Course Management Apps")
+st.header("Assignment")
+st.subheader("Assignments Manager")
+
+next_assignment_id_number=3
+
+
+if json_path.exists:
+    with json_path.open("r", encoding="utf-8") as f:
+        assignments=json.load(f)
+
+tab1, tab2, tab3= st.tabs(["View Assignments", "Add new Assignments", "Update an Assignment"])
+
+with tab1:
+    st.info("This tab is underdeveloped")
+    option= st.radio("View/Search", ["View", "Search"], horizontal=True)
+
+    if option=="View":
+        st.dataframe(assignments)
+
+    else:
+        search_title=st.selectbox("Assignment Titles", [])
+
+with tab2:
+     title= st.text_input("Title", placeholder="ex. Homework 1", help="This is the name of the assignment")
+     description=st.text_area("Description", placeholder="ex. database design....")
+    due_date=st.date_input("Due Date")
+ assignments_type=st.radio("Type",["Homework","Lab"])
+ assignments_type2=st.selectbox("Type",["Homework","Lab"])
+ if assignments_type2 == "Other":
+    assignments_type2=st.text_input("Assignment Type")
+    
+assignment_type3= st.checkbox("Assignment Type")
+
+labs=st.checkbox("Lab")
+
+with st.expander("Assignment Preview",expanded=True):
+    st.markdown("## Live Preview")
+    st.markdown(f"Title: {title}")
+
+btn_save=st.button("Save", use_container_width=100, disabled=False)
+
+
+json_path=Path("assignments.json")
+
+
+if btn_save:
+    with st.spinner("Saving the Assignment"):
+        time.sleep(5)
+        if title=='':
+            st.warning("Enter Assignment Title")
+        else:
+             #Create a new assignment 
+            new_assignment_id="HW_" +str(next_assignment_id_number)
+            next_assignment_id_number +=1
+            
+            assignments.append(
+                {"id": new_assignment_id,
+                 "title": title,
+                 "description": description,
+                   "points": 100,
+                   "type": assignments_type})
+
+
+            with json_path.open("w",encoding="utf-8") as f:
+                json.dump(assignments,f)
+            
+            st.success("Assignment is recorded!")
+            st.dataframe(assignments)
+
+
+
+
+st.divider()
+
+assignments=[
+    {    "id":"HW1",
+        "title":"Introduction to Database",
+        "description": "basics of database design",
+        "points":100,
+        "type":"homework"},
+    
+    {
+        "id": "HW2",
+        "title":"Normalization",
+        "description": "Normalize the table design",
+        "points":100,
+        "type":"lab"}
+]
+
+# Add new assignment 
+st.markdown("# Add New Assignment")
+
+#input
+title= st.text_input("Title", placeholder="ex. Homework 1", help="This is the name of the assignment")
+
+description=st.text_area("Description", placeholder="ex. database design....")
+due_date=st.date_input("Due Date")
+assignments_type=st.radio("Type",["Homework","Lab"])
+#assignments_type2=st.selectbox("Type",["Homework","Lab"])
+#if assignments_type2 == "Other":
+#assignments_type2=st.text_input("Assignment Type")
+
+#assignment_type3= st.checkbox("Assignment Type")
+
+#labs=st.checkbox("Lab")
+
+with st.expander("Assignment Preview",expanded=True):
+    st.markdown("## Live Preview")
+    st.markdown(f"Title: {title}")
+
+btn_save=st.button("Save", use_container_width=100, disabled=False)
+
+import time 
+
+import json
+from pathlib import Path 
+
+json_path=Path("assignments.json")
+
+
+if btn_save:
+    with st.spinner("Saving the Assignment"):
+        time.sleep(5)
+        if title=='':
+            st.warning("Enter Assignment Title")
+        else:
+             #Create a new assignment 
+            new_assignment_id="HW_" +str(next_assignment_id_number)
+            next_assignment_id_number +=1
+            
+            assignments.append(
+                {"id": new_assignment_id,
+                 "title": title,
+                 "description": description,
+                   "points": 100,
+                   "type": assignments_type})
+
+
+            with json_path.open("w",encoding="utf-8") as f:
+                json.dump(assignments,f)
+            
+            st.success("Assignment is recorded!")
+            st.dataframe(assignments)
---
+++ app_day6.py
@@ -0,0 +1,134 @@
+import streamlit as st
+import json
+from pathlib import Path
+from datetime import datetime
+import uuid
+import time
+
+st.set_page_config(page_title="Course Manager", layout="centered")
+
+json_file = Path("users.json")
+users = []
+if json_file.exists():
+    with open(json_file, "r") as f:
+        users = json.load(f)
+        
+
+#login
+
+st.set_page_config(page_title="Course Manager", layout="centered")
+st.title("Course Manager App")
+
+if "logged_in" not in st.session_state:
+    st.session_state["logged_in"]==False
+    
+if "user" not in st.session_state:
+    st.session_state["user"]==None
+
+if "page" not in st.session_state:
+    st.session_state["page"]="login"
+
+if "role" not in st.session_state:
+    st.session_state("role")==None
+
+users= [
+    {"id":"1",
+     "email":" admin@school.edu",
+     "password":"ABC123",
+     "role":"admin",
+     "registered_at":",,,"}
+]
+
+json_path_assignment=Path("assignments.json")
+
+if json_path_assignment.exists():
+    with open(json_path_assignment, "r", encoding="utf-8") as f:
+        assignments = json.load(f)
+
+if st.session_state["role"]== "Admin":
+    st.markdown("This is the Admin UI-Dashboard")
+
+elif st.session_state["role"]== "Instructor":
+    st.markdown("This is the instructor UI-Dashboard")
+    if st.session_state["page"]== "home":
+        st.markdown("Welcome (st.session_state['user']['email']) to your dashboard")
+        if st.button("Go to Dashboard", types="primary", key="view_dash_btn"):
+            st.session_state("page")="dashboard"
+            st.rerun()
+    elif st.session_state["page"]== "dashboard":
+
+
+    
+    if st.button("Log out", type="primary", use_container_width= True):
+        with st.spinner("loggin out..."):
+            st.session_state["logged_in"] = False
+            st.session_state["user"] = None
+            st.session_state["role"] = None
+            st.session_state["page"]= "home"
+            time.sleep(4)
+            st.rerun()
+
+st.subheader("Log In")
+with st.container(border=True):
+    email_input = st.text_input("Email", key="login_email")
+    password_input = st.text_input("Password", type="password", key="login_password")
+    
+    if st.button("Log In", type="primary", use_container_width=True):
+        with st.spinner("Logging in..."):
+            time.sleep(2) # Fake backend delay
+            
+            # Find user
+            found_user = None
+            for user in users:
+                if user["email"].strip().lower() == email_input.strip().lower() and user["password"] == password_input:
+                    found_user = user
+                    break
+            
+            if found_user:
+                st.success(f"Welcome back, {found_user['email']}!")
+                st.session_state["logged_in"]==True
+                st.session_state["user"]=found_user
+                st.session_state["role"]=found_user["role"]
+                time.sleep(2)
+                st.rerun()
+            else:
+                st.error("Invalid credentials")
+
+
+# Registration 
+st.subheader("New Instructor Account")
+with st.container(border=True):
+    new_email = st.text_input("Email Address", key="reg_email")
+    new_password = st.text_input("Password", type="password", key="reg_password")
+    
+    if st.button("Create Account"):
+        with st.spinner("Creating account..."):
+            time.sleep(2) # Fake backend delay
+            # ... (Assume validation logic here) ...
+            users.append({
+                "id": str(uuid.uuid4()),
+                "email": new_email,
+                "password": new_password,
+                "role": "Instructor"
+            })
+            #with open(json_file, "w") as f:
+               #json.dump(users, f, indent=4)
+            st.success("Account created!")
+            st.rerun()
+
+st.write("---")
+st.dataframe(users)
+
+with st.sidebar:
+    if "logged_in" in st.session_state and st.session_state["logged_in"] != None:
+        user=st.session_state["user"]
+        st.markdown(f"Welcome {user['full_name']}")
+    else:
+        st.markdown("Welcome! - Login")
+
+        if btn_save:
+            with st.spinner("Saving the Assignment..."):
+                time.sleep(5) 
+                if not st.title:
+                    
+                
